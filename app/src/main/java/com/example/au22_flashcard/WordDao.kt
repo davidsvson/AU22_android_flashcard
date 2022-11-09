414cfae20356040f33fcf43c3e0cc4650cbd6e59
Jonas B <jonas.backas@iths.se>
2022-11-09 09:43:27 +0100

    database created

---
+++ app/src/main/java/com/example/au22_flashcard/WordDao.kt
@@ -0,0 +1,21 @@
+package com.example.au22_flashcard
+
+import androidx.room.Dao
+import androidx.room.Insert
+
+@Dao
+interface WordDao {
+
+    @Insert
+    fun insert(word: Word)
+
+    //delete
+
+    //getAllwords
+
+    //
+
+
+
+
+}
