fcbc5fdb324e3ed2dd0a2523ae4e4224bdcd4ce1
ciliaence <cilia.ence@telia.com>
2022-11-09 12:42:39 +0100

    added room

---
+++ app/src/main/java/com/example/au22_flashcard/WordDao.kt
@@ -0,0 +1,15 @@
+package com.example.au22_flashcard
+
+import androidx.room.Dao
+import androidx.room.Insert
+
+@Dao
+interface WordDao {
+
+    @Insert
+    fun insert (word: Word)
+
+    //delete
+
+    //getallwords
+}
